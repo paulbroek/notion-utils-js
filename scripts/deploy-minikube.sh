544b4b8b3ab4156232971642ac6366280f5dc0eb
paulbroek <pauliusbroek@hotmail.com>
2022-12-04 05:40:16 +0100

    add deploy-minikube.sh script #2

--- scripts/deploy-minikube.sh
+++ scripts/deploy-minikube.sh
@@ -7,6 +7,8 @@ sed -i "s|<TELEGRAM_BOT_TOKEN>|$TELEGRAM_BOT_TOKEN|" $TMP_DIR/deployment.yml
 sed -i "s|<NOTION_DATABASE_ID>|$NOTION_DATABASE_ID|" $TMP_DIR/deployment.yml
 sed -i "s|<DATABASE_URL>|$DATABASE_URL|" $TMP_DIR/deployment.yml
 
+TAG=main && sed -i 's|<IMAGE>|ghcr.io/paulbroek/notion-utils-js:'${TAG}'|' $TMP_DIR/deployment.yml
+
 kubectl apply -f $TMP_DIR/deployment.yml
 
 kubectl rollout status deployment/notion-telegram-bot-js
