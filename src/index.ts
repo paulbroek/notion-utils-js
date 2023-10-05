be3c71dbe19aad03321a1dc87a5e814e9645678a
Paul Broek <pauliusbroek@hotmail.com>
2023-10-05 14:22:32 +0200

    log uniquecol

--- src/index.ts
+++ src/index.ts
@@ -326,7 +326,7 @@ const violatesUniqueRows = async (
   databaseId: string
 ): Promise<Boolean> => {
   const uniqueCol: string = COLLECTIONS[collection].UNIQUE_COLUMN;
-  console.log(`uniqueCol=${uniqueCol}`);
+  console.error(`uniqueCol=${uniqueCol}`);
   const response = await notion.databases.query({
     database_id: databaseId,
     filter: {
